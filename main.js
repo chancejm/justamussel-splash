6bf97ca2181cdec6516697e0349370793ef44911
chancejm <37995184+chancejm@users.noreply.github.com>
2025-05-18 21:03:38 -0700

    Basic Logo / Text
    WIP

---
+++ main.js
@@ -0,0 +1,6 @@
+ window.addEventListener('DOMContentLoaded', () => {
+    const logo = document.getElementById('logo');
+    setTimeout(() => {
+      //logo.classList.add('slide-in-elliptic-bottom-bck');
+    }, 0); // adjust to match roll-in-left duration
+  });
